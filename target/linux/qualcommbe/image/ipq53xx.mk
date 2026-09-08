openwrt/openwrt

qualcommbe: add support for Ubiquiti UniFi 7 Pro XGS
Add support for the Ubiquiti UniFi 7 Pro XGS ceiling-mounted
access point.

Device specification
--------------------

SoC:                    Qualcomm IPQ5332, quad-core Cortex-A53, 1.5 GHz
RAM:                    2 GB
Flash:                  MT3204 eMMC, 4 GB
Ethernet:               1x 10GbE RJ45
WiFi:                   WiFi 7, 8 spatial streams
                        2.4 GHz 2x2, 5 GHz 4x4, 6 GHz 2x2
Power:                  PoE++, 29 W max.
LEDs:                   1x white status, 1x blue WLAN
Console:                ttyMSM0 at 115200 8n1
Bootloader:             U-Boot

Installing OpenWrt
------------------

1. Boot the device into the vendor firmware.

   The factory firmware should be reachable via SSH at 192.168.1.20
   with user ubnt and password ubnt. If the device has already been
   adopted, the SSH credentials can be found in the UniFi WebUI under
   Devices -> Device Updates and Settings -> Device SSH Settings.

2. Check the vendor firmware and U-Boot versions.

   The firmware version can be checked in the SSH banner or via:

     cat /usr/lib/semver

   The U-Boot version can be checked via:

     strings /dev/mtd8 | grep '^U-Boot '

3. If the device runs firmware newer than 8.0.23 or U-Boot newer than
   2024.01 v1.0.51.63, downgrade first to 8.0.23.

   Newer U-Boot versions enforce a signature check while loading the
   image, which prevents booting OpenWrt.

   Download:
   https://dl.ui.com/unifi/firmware/U7PRO/8.0.23.16670/BZ.ipq53xx_8.0.23+16670.250316.0909.bin

   Downgrade via SSH by copying the image to /tmp/fwupdate.bin and
   running:

     fwupdate -c
     syswrapper.sh upgrade2

   Or use the UniFi WebUI:
   Devices -> <select AP> -> Manage -> Manual Firmware Update

   After the AP reboots, log in again and repeat the checks from step 2.
   Continue only once the device reports firmware 8.0.23 and U-Boot
   2024.01 v1.0.51.63 or older.

4. Copy the initramfs image to /tmp via SCP.

5.

target/linux/qualcommbe/image/ipq53xx.mk
@@ -1,0 +1,42 @@
+
+define Build/fit-inline-rootfs
+	rm -f $@.dtb $@.kernel
+	cp $@ $@.kernel
+	cp $(word 2,$(1)) $@.dtb
+	cp $@.kernel $@
+	$(call Build/fit-its,$(word 1,$(1)) $@.dtb with-rootfs)
+	$(call Build/fit-image,$(word 1,$(1)) $@.dtb with-rootfs)
+	rootfs_offset="$$(grep -oba hsqs $@ | head -n1 | cut -d: -f1)"; \
+	[ -n "$$rootfs_offset" ] || { echo "Failed to locate SquashFS in $@"; exit 1; }; \
+	pad="$$(( (4096 - ($$rootfs_offset % 4096)) % 4096 ))"; \
+	cp $(word 2,$(1)) $@.dtb; \
+	dd if=/dev/zero bs=1 count="$$pad" >> $@.dtb 2>/dev/null; \
+	cp $@.kernel $@; \
+	$(call Build/fit-its,$(word 1,$(1)) $@.dtb with-rootfs)
+	$(call Build/fit-image,$(word 1,$(1)) $@.dtb with-rootfs)
+	rm -f $@.dtb $@.kernel
+endef
+
+define Device/ubnt_u7-pro-xgs
+	DEVICE_VENDOR := Ubiquiti
+	DEVICE_MODEL := UniFi 7
+	DEVICE_VARIANT := Pro XGS
+	# Stock U-Boot probes config-a6a4 on this board.
+	DEVICE_DTS_CONFIG := config-a6a4
+	SOC := ipq5332
+	SUPPORTED_DEVICES += qcom,ipq5332-ap-mi01.2-qcn9160-c1-u7-pro-xgs
+	DEVICE_PACKAGES := e2fsprogs f2fsck fitblk mkf2fs \
+		kmod-ath12k ath12k-firmware-qcn9274 \
+		ipq-wifi-ubnt_u7-pro-xgs kmod-leds-pwm \
+		kmod-phy-realtek rtl8261n-firmware
+	KERNEL := kernel-bin | lzma
+	KERNEL_INITRAMFS := kernel-bin | lzma | \
+		fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+	KERNEL_INITRAMFS_SUFFIX := .itb
+	IMAGE_SIZE := 128m
+	IMAGES := sysupgrade.itb
+	IMAGE/sysupgrade.itb := append-kernel | \
+		fit-inline-rootfs lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb | \
+		check-size | append-metadata
+endef
+TARGET_DEVICES += ubnt_u7-pro-xgs

cvediver/openwrt-flint3

target/linux/qualcommbe/image/ipq53xx.mk
@@ -1,0 +1,42 @@
+
+define Build/fit-inline-rootfs
+	rm -f $@.dtb $@.kernel
+	cp $@ $@.kernel
+	cp $(word 2,$(1)) $@.dtb
+	cp $@.kernel $@
+	$(call Build/fit-its,$(word 1,$(1)) $@.dtb with-rootfs)
+	$(call Build/fit-image,$(word 1,$(1)) $@.dtb with-rootfs)
+	rootfs_offset="$$(grep -oba hsqs $@ | head -n1 | cut -d: -f1)"; \
+	[ -n "$$rootfs_offset" ] || { echo "Failed to locate SquashFS in $@"; exit 1; }; \
+	pad="$$(( (4096 - ($$rootfs_offset % 4096)) % 4096 ))"; \
+	cp $(word 2,$(1)) $@.dtb; \
+	dd if=/dev/zero bs=1 count="$$pad" >> $@.dtb 2>/dev/null; \
+	cp $@.kernel $@; \
+	$(call Build/fit-its,$(word 1,$(1)) $@.dtb with-rootfs)
+	$(call Build/fit-image,$(word 1,$(1)) $@.dtb with-rootfs)
+	rm -f $@.dtb $@.kernel
+endef
+
+define Device/ubnt_u7-pro-xgs
+	DEVICE_VENDOR := Ubiquiti
+	DEVICE_MODEL := UniFi 7
+	DEVICE_VARIANT := Pro XGS
+	# Stock U-Boot probes config-a6a4 on this board.
+	DEVICE_DTS_CONFIG := config-a6a4
+	SOC := ipq5332
+	SUPPORTED_DEVICES += ubnt,u7-pro-xgs
+	DEVICE_PACKAGES := e2fsprogs f2fsck fitblk mkf2fs \
+		kmod-ath12k ath12k-firmware-qcn9274 \
+		ipq-wifi-ubnt_u7-pro-xgs kmod-leds-pwm \
+		kmod-phy-realtek rtl826x-firmware
+	KERNEL := kernel-bin | lzma
+	KERNEL_INITRAMFS := kernel-bin | lzma | \
+		fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+	KERNEL_INITRAMFS_SUFFIX := .itb
+	IMAGE_SIZE := 128m
+	IMAGES := sysupgrade.itb
+	IMAGE/sysupgrade.itb := append-kernel | \
+		fit-inline-rootfs lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb | \
+		check-size | append-metadata
+endef
+TARGET_DEVICES += ubnt_u7-pro-xgs